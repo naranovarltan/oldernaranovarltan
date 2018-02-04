7fb18ed63cd5bffc9ddae6883d0d3fbeeb642c34
Arltan <a.naranov@dunice.net>
2018-02-05 00:31:37 +0300

    nodemodeules

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
+const    gulp = require('gulp'),
+ autoprefixer = require('gulp-autoprefixer'),
+   livereload = require('gulp-livereload');
  
-gulp.task('default', () =>
-    gulp.src('src/style.css')
+
+gulp.task('livereload', function(){
+    gulp.src('CSS/style.css')
         .pipe(autoprefixer({
             browsers: ['last 5 versions'],
             cascade: false
         }))
         .pipe(gulp.dest('dist'))
-);
+        .pipe(livereload());
+});
+
+gulp.task('default', () => {
+    livereload.listen();
+    gulp.watch('CSS/style.css', ['livereload']);
+});
